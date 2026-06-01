0608e4018ac12cf381c47e951be16b98e38df071
Laurie Blackman <laurie.blackman@gmail.com>
2026-06-01 22:23:53 +0100

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).end();
   try {
     const response = await fetch('https://api.anthropic.com/v1/messages', {
